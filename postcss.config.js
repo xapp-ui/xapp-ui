8d51769de33cbb5cb302679ebbd49fa7021f8bd5
Raf <7909000@gmail.com>
2019-07-06 14:07:08 +0500

    add purgecss plugin to keep file size small

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,22 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './src/components/**/*.html',
+    './src/components/**/*.vue',
+    './src/components/**/*.js',
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
