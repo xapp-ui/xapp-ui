2df742f23c6dda9657bc18b8b24c2f2ecf2c081a
Raf <7909000@gmail.com>
2019-07-08 00:39:13 +0500

    update xbutton component features
    added cleaner outline for button focus state

--- docs/components/XButton/XButton.md
+++ docs/components/XButton/XButton.md
@@ -2,8 +2,8 @@
 
 The ```x-button``` component can be used instead of an html standard button.
 
-### Usage
----
+### Basic Examples
+***
 
 <x-button>Default</x-button>
 <x-button variant='primary'>Primary</x-button>
@@ -23,15 +23,49 @@ The ```x-button``` component can be used instead of an html standard button.
 <x-button variant='info'>Info</x-button>
 ```
 
-<x-button variant='info' size='large'>LARGE Button</x-button>
-<x-button variant='info' size='small'>SMALL Button</x-button>
+### Outline buttons
+***
+<x-button outline>Default</x-button>
+<x-button outline variant='primary'>Primary</x-button>
+<x-button outline variant='secondary'>Secondary</x-button>
+<x-button outline variant='success'>Success</x-button>
+<x-button outline variant='warning'>Warning</x-button>
+<x-button outline variant='danger'>Danger</x-button>
+<x-button outline variant='info'>Info</x-button>
+
 ```html
-<x-button variant='info' size='large'>LARGE Button</x-button>
-<x-button variant='info' size='small'>SMALL Button</x-button>
+<x-button outline>Default</x-button>
+<x-button outline variant='primary'>Primary</x-button>
+<x-button outline variant='secondary'>Secondary</x-button>
+<x-button outline variant='success'>Success</x-button>
+<x-button outline variant='warning'>Warning</x-button>
+<x-button outline variant='danger'>Danger</x-button>
+<x-button outline variant='info'>Info</x-button>
 ```
-### Props
----
-| Prop name | Type   | Default | Description                              |
-|-----------|--------|---------|------------------------------------------|
-| variant   | String | Default | Button variant to apply specific styles. |
-| size      | String | normal  | Defines one of ```normal, small, medium, large``` as size|
+
+### Button sizes
+***
+<x-button variant='info' size='xlarge'>EXTRA LARGE</x-button>
+<x-button variant='info' size='large'>LARGE</x-button>
+<x-button variant='info' size='normal'>NORMAL</x-button>
+<x-button variant='info' size='small'>SMALL</x-button>
+<x-button variant='info' size='xsmall'>EXTRA SMALL</x-button>
+```html
+<x-button variant='info' size='xlarge'>EXTRA LARGE</x-button>
+<x-button variant='info' size='large'>LARGE</x-button>
+<x-button variant='info' size='normal'>NORMAL</x-button>
+<x-button variant='info' size='small'>SMALL</x-button>
+<x-button variant='info' size='xsmall'>EXTRA SMALL</x-button>
+```
+
+### Button Elements
+***
+<x-button element='button'>Button</x-button>
+<x-button element='a' href='#/btn-link'>Link</x-button>
+<x-button element='input' value='Submit' type='submit' />
+
+```html
+<x-button element='button'>Button</x-button>
+<x-button element='a' href='#/btn-link'>Link</x-button>
+<x-button element='input' value='Submit' type='submit' />
+```
--- src/components/XButton/XButton.vue
+++ src/components/XButton/XButton.vue
@@ -1,25 +1,37 @@
 <template>
     <component :is="element" v-bind="$attrs" v-on="$listeners" :class="{
         'x-button': true,
-        'bg-white': variant === 'default',
-        'hover:bg-gray-lightest': variant === 'default',
-        'bg-primary' : variant === 'primary',
-        'hover:bg-primary-dark' : variant === 'primary',
-        'bg-secondary' : variant === 'secondary',
-        'hover:bg-secondary-dark' : variant === 'secondary',
-        'bg-warning' : variant === 'warning',
-        'hover:bg-warning-dark' : variant === 'warning',
-        'bg-danger' : variant === 'danger',
-        'hover:bg-danger-dark' : variant === 'danger',
-        'bg-info' : variant === 'info',
-        'hover:bg-info-dark' : variant === 'info',
-        'bg-success' : variant === 'success',
-        'hover:bg-success-dark' : variant === 'success',
+        'bg-white': variant === 'default' || outline === true,
+        'hover:bg-gray-lighter': variant === 'default',
+        'bg-primary' : variant === 'primary' && outline === false,
+        'text-primary' : variant === 'primary' && outline === true,
+        'hover:bg-primary-light' : variant === 'primary' && outline === true,
+        'hover:bg-primary-dark' : variant === 'primary' && outline === false,
+        'bg-secondary' : variant === 'secondary' && outline === false,
+        'text-secondary' : variant === 'secondary' && outline === true,
+        'hover:bg-secondary-light' : variant === 'secondary' && outline === true,
+        'hover:bg-secondary-dark' : variant === 'secondary' && outline === false,
+        'bg-warning' : variant === 'warning' && outline === false,
+        'text-warning' : variant === 'warning' && outline === true,
+        'hover:bg-warning-light' : variant === 'warning' && outline === true,
+        'hover:bg-warning-dark' : variant === 'warning' && outline === false,
+        'bg-danger' : variant === 'danger' && outline === false,
+        'text-danger' : variant === 'danger' && outline === true,
+        'hover:bg-danger-light' : variant === 'danger' && outline === true,
+        'hover:bg-danger-dark' : variant === 'danger' && outline === false,
+        'bg-info' : variant === 'info' && outline === false,
+        'text-info' : variant === 'info' && outline === true,
+        'hover:bg-info-light' : variant === 'info' && outline === true,
+        'hover:bg-info-dark' : variant === 'info' && outline === false,
+        'bg-success' : variant === 'success' && outline === false,
+        'text-success' : variant === 'success' && outline === true,
+        'hover:bg-success-light' : variant === 'success' && outline === true,
+        'hover:bg-success-dark' : variant === 'success' && outline === false,
         'text-white': variant !== 'default' && !textDark,
         'text-black' : variant === 'default' || textDark === true,
         [size] : true
     }">
-        <slot></slot>
+        <slot v-if="element !== 'input'"></slot>
     </component>
 </template>
 <script>
@@ -28,7 +40,10 @@
         props: {
             element: {
                 type: String,
-                default: 'button'
+                default: 'button',
+                validator: (val) => {
+                    return ['input', 'button', 'a'].indexOf(val) !== -1;
+                }
             },
 
             variant: {
@@ -37,6 +52,11 @@
                 validator: (val) => {
                     return ['default', 'primary', 'secondary', 'warning', 'danger', 'info', 'success'].indexOf(val) !== -1;
                 }
+            },
+
+            outline: {
+                type: Boolean,
+                default: false
             },
 
             textDark: {
@@ -48,7 +68,7 @@
                 type: String,
                 default: 'normal',
                 validator: (val) => {
-                    return ['normal', 'small', 'large'].indexOf(val) !== -1;
+                    return ['normal', 'small', 'xsmall', 'large', 'xlarge'].indexOf(val) !== -1;
                 }
             }
         }
